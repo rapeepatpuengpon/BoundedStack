11a7346a40514e81440984d0c4a42a4f649225d1
rapeepatpuengpon <rapeepat.pu@ku.th>
2026-07-26 08:10:09 +0700

    Add files via upload

--- TestRunner.java
+++ TestRunner.java
@@ -5,4 +5,44 @@ public class TestRunner {
         if (ok) {pass++; System.out.println(" [pass] " + name);}
         else    {fail++; System.out.println(" [fail] " + name);}
     }
+
+    public static void main() {
+        System.out.println("=== Test ===");
+
+        testCreater();
+        testAdd();
+        testRemove();
+        testObservers();
+        testProducer();
+        testExposure();
+
+        System.out.println(" [pass] = " + pass );
+        System.out.println(" [fail] = " + fail );
+        
+    }
+
+    private static void testCreater() {
+        
+    }
+    
+    private static void testAdd() {
+        
+    }
+
+    private static void testRemove() {
+        
+    }
+
+    private static void testObservers() {
+        
+    }
+
+    private static void testProducer() {
+        
+    }
+
+    private static void testExposure() {
+        
+    }
+
 }
