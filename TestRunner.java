64597591aac7ca64b15e5a6499b90da029a445d1
rapeepatpuengpon <rapeepat.pu@ku.th>
2026-07-26 07:37:12 +0700

    Add files via upload

--- TestRunner.java
+++ TestRunner.java
@@ -1,3 +1,8 @@
 public class TestRunner {
-    
+    static int pass = 0, fail = 0;
+
+    static void check(String name , boolean ok) {
+        if (ok) {pass++; System.out.println(" [pass] " + name);}
+        else    {fail++; System.out.println(" [fail] " + name);}
+    }
 }
